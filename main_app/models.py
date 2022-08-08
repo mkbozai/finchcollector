8c28ab374916d3c73633ef50783099432d4aa4a8
mkbozai <munaaf24@gmail.com>
2022-08-08 16:57:49 -0500

    create finch model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    habitat = models.TextField(max_length=250)
+    description = models.TextField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Finch:
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Gary', 'spice finch', 'not afraid to make things spicy', 6),
-  Finch('Chirp', 'society finch', 'always chirping at folks', 2),
-  Finch('Nelly', 'strawberry finch', 'looks the best', 0),
-  Finch('Chris', 'blue finch', 'head coach of the timberwolves', 3),
-]
-
 
 # Create your views here.
 
