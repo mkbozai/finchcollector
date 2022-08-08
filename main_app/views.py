ddc9256b3330cd836d22b7bd88323de0878aa224
mkbozai <munaaf24@gmail.com>
2022-08-07 22:56:24 -0500

    updated index content

--- main_app/views.py
+++ main_app/views.py
@@ -19,7 +19,7 @@ finches = [
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Master Finch Collector</h1>')
+    return HttpResponse('<img src="https://media.istockphoto.com/vectors/set-of-colorful-birds-isolated-on-white-background-vector-id1155378512?k=20&m=1155378512&s=612x612&w=0&h=8mKT5zEt0XtyvjLss0UkpVM3buJqsPH2aKJKZwD3n3Y=">')
 
 def about(request):
     return render(request, 'about.html')
